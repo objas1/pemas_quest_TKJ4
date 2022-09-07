5a9cdd7c15a3e427ea33d9e24732ed17b37649da
objas1 <112526442+objas1@users.noreply.github.com>
2022-09-07 18:23:13 +1100

    Add files via upload

---
+++ python8-tkj4py.py
@@ -0,0 +1,17 @@
+list_integer = [1, 2, 3, 10, 100, 250]
+list_string = ["RIVAI", "FAISAL", "UNTUNG", "ANJAS",]
+list_mix = [20, "Arya", True, "Anjas"]
+
+print("Data sebelum diubah :",list_string)
+list_string[0] = "Miya1"
+list_string[1] = "Miya2"
+list_string[2] = "Miya3"
+list_string[3] = "Miya4"
+print("Data setelah diubah:",list_string)
+
+print("Data sebelum diubah :",list_mix)
+list_mix[0] = "Miya1"
+list_mix[1] = "Miya2"
+list_mix[2] = "Miya3"
+list_mix[3] = "Miya4"
+print("Data setelah diubah:",list_mix)
